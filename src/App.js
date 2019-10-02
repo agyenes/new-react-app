70ff939709b381e44eff8e45c0dfb589b957d3c9
agyenes <aron.gyenes@gmail.com>
2019-10-02 14:53:04 +0200

    add two way binding

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},      
+      { name: 'John', age: 21},      
+      { name: 'Dieter', age: 25}
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'New name', age: 28},      
+        { name: 'John', age: 21},      
+        { name: 'Dieter', age: 87}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},      
+        { name: event.target.value, age: 21},      
+        { name: 'Dieter', age: 25}
+      ]
+    })
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'yellow',
+      border: '1px solid blue',
+      padding: '8px'
+    };
+
+    return (
+      <div className="App">
+        <h1>This is the App aaaa</h1>
+        <button style={ style } onClick={this.switchNameHandler}>Switch</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}            
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}>my hobbies are: rowing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>my hobbies are: rowing</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// create-react-app my-app --scripts-version 1.1.5
